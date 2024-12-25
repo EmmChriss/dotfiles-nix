88a7c71443c0f5efc9180d85ab71409c83c1b20b
EmmChriss <emmchris@protonmail.com>
2024-12-25 13:13:57 +0200

    waybar.service should point to hyprland-session.target

--- home/waybar.nix
+++ home/waybar.nix
@@ -15,7 +15,10 @@
 
   programs.waybar = {
     enable = true;
-    systemd.enable = true;
+    systemd = {
+      enable = true;
+      target = "hyprland-session.target";
+    };
     settings.mainBar = {
       layer = "top";
       position = "top";
