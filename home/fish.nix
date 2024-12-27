dfd231778fd67495e185cd680e4280c898a0aeb3
EmmChriss <emmchris@protonmail.com>
2024-12-27 15:05:32 +0200

    add top-level shell greeting

--- home/fish.nix
+++ home/fish.nix
@@ -1,25 +1,40 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
-    # enable fish shell
-    programs.fish = {
-      enable = true;
-      plugins = (with pkgs.fishPlugins; [
-        # Colorized nixpkgs command output
-        { name = "grc"; src = grc.src; }
-        # Hydro prompt
-        { name = "hydro"; src = hydro.src; }
-        # Import foregin envs (aka. bash or conf)
-        { name = "foreign-env"; src = foreign-env.src; }
-        # Receive notifications when process done
-        { name = "done"; src = done.src; }
-        # Colored man-pages
-        { name = "colored-man"; src = colored-man-pages.src; }
-      ]);
-      functions = {
-        # disable greeting
-        fish_greeting = "";
-      };
-      interactiveShellInit = builtins.readFile ./config/config.fish;
+let
+    greetings = pkgs.writeShellApplication {
+      name = "greetings";
+      runtimeInputs = with pkgs; [ neo-cowsay fortune ];
+      text = ''
+        fortune | cowthink --rainbow --bold -n
+        cat <<- EOF
+        Hello $USER!
+        <c-f> to open file manager
+        <c-t> to fuzzy search files
+        EOF
+      '';
     };
+  in
+{
+  home.packages = [ greetings ];
+
+  # enable fish shell
+  programs.fish = {
+    enable = true;
+    plugins = (with pkgs.fishPlugins; [
+      # Colorized nixpkgs command output
+      { name = "grc"; src = grc.src; }
+      # Hydro prompt
+      { name = "hydro"; src = hydro.src; }
+      # Import foregin envs (aka. bash or conf)
+      { name = "foreign-env"; src = foreign-env.src; }
+      # Receive notifications when process done
+      { name = "done"; src = done.src; }
+      # Colored man-pages
+      { name = "colored-man"; src = colored-man-pages.src; }
+    ]);
+    functions.fish_greeting = "";
+    interactiveShellInit = builtins.readFile ./config/config.fish + ''
+      test -z "$FISH_TOP" && ${lib.getExe greetings} && set -x FISH_TOP 1
+    '';
+  };
 }
