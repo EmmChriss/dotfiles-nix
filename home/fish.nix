c7a88f3dd26811e30535fa0064b313e5c1f8886f
EmmChriss <emmchris@protonmail.com>
2025-02-03 15:40:27 +0200

    mark nix-shells in fish prompt

--- home/fish.nix
+++ home/fish.nix
@@ -1,19 +1,27 @@
 { pkgs, lib, ... }:
 
 let
-    greetings = pkgs.writeShellApplication {
-      name = "greetings";
-      runtimeInputs = with pkgs; [ neo-cowsay fortune ];
-      text = ''
-        fortune | cowthink --rainbow --bold -n
-        cat <<- EOF
-        Hello $USER!
-        <c-f> to open file manager
-        <c-t> to fuzzy search files
-        EOF
-      '';
-    };
-  in
+  latestHydro = pkgs.fishPlugins.hydro.overrideAttrs (old: {
+      src = pkgs.fetchFromGitHub {
+        owner = "jorgebucaran";
+        repo = "hydro";
+        rev = "9c93b89573bd722f766f2190a862ae55e728f6ba";
+        hash = "sha256-QYq4sU41/iKvDUczWLYRGqDQpVASF/+6brJJ8IxypjE=";
+      };
+  });
+  greetings = pkgs.writeShellApplication {
+    name = "greetings";
+    runtimeInputs = with pkgs; [ neo-cowsay fortune ];
+    text = ''
+      fortune | cowthink --rainbow --bold -n
+      cat <<- EOF
+      Hello $USER!
+      <c-f> to open file manager
+      <c-t> to fuzzy search files
+      EOF
+    '';
+  };
+in
 {
   home.packages = [ greetings ];
 
@@ -24,7 +32,7 @@ let
       # Colorized nixpkgs command output
       { name = "grc"; src = grc.src; }
       # Hydro prompt
-      { name = "hydro"; src = hydro.src; }
+      { name = "hydro"; src = latestHydro.src; }
       # Import foregin envs (aka. bash or conf)
       { name = "foreign-env"; src = foreign-env.src; }
       # Receive notifications when process done
@@ -34,9 +42,18 @@ let
     ]);
     functions.fish_greeting = "";
 
+    shellAliases = {
+      "," = "FISH_NIX_SHELL=1 command ,";
+      nix-shell = "FISH_NIX_SHELL=1 command nix-shell";
+    };
+
     # TODO: check out Hydro documentation
     # See: https://github.com/jorgebucaran/hydro
     interactiveShellInit = ''
+      # mark nix shells
+      set --global hydro_color_start "--bold"
+      test -n "$FISH_NIX_SHELL" && set --global hydro_symbol_start "SHELL "
+    
       # mark subshells with FISH_TOP=1
       test -z "$FISH_TOP" && ${lib.getExe greetings} && set -x FISH_TOP 1
 
